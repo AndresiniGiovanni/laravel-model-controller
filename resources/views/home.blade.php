ea97cc76eb70452d82d8a6e8feced5bc42c605fa
AndresiniGiovanni <andresini.giovanni@gmail.com>
2022-12-16 16:53:52 +0100

    alcuni style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,15 +9,14 @@
       <div class="row d-flex justify-content-center">
           @foreach ($movie as $movie )
       <div class="card bg-transparent col-lg-3 col-md-6 border-0 mb-5">
-        <img class="card-img-top rounded-3 " src="{{ $movie->image }}" alt="" />
-        <div class="mt-2 my-title text-uppercase text-center">{{ $movie['title'] }}</div>
+        <img class="card-img-top rounded-3  my-card " src="{{ $movie->image }}" alt="" />
+        <div class="mt-3 my-title text-uppercase text-center">{{ $movie['title'] }}</div>
       </div>
     @endforeach
       </div>
   </div>
 </div>
 </div>
-
 @endsection
 
 
