6e88058539e6eab1e7280c4c58a089dddfa75380
AndresiniGiovanni <andresini.giovanni@gmail.com>
2022-12-16 16:38:47 +0100

    migliorate le cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,19 +2,24 @@
 @section('titolo', 'Lista dei film')
 @section('content')
 
-<h1 class="text-center pt-3 ">Home Movie</h1>
+<h1 class="text-center pt-4 pb-5 ">Home Movie</h1>
 
-   <div class="container-fluid  mt-5">
-      <div class="row ms-5 me-5">
-      @foreach ($movie as $movie )
-      <div class="my-card card  bg-transparent col-3 border-0 mb-5">
+ <div class="container">
+  <div class="row">
+      <div class="row d-flex justify-content-center">
+          @foreach ($movie as $movie )
+      <div class="card bg-transparent col-lg-3 col-md-6 border-0 mb-5">
         <img class="card-img-top rounded-3 " src="{{ $movie->image }}" alt="" />
         <div class="mt-2 my-title text-uppercase text-center">{{ $movie['title'] }}</div>
       </div>
     @endforeach
-    </div>
+      </div>
   </div>
+</div>
+</div>
+
 @endsection
+
 
 
 
